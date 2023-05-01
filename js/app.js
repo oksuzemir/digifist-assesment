0d89af5187b8338e07d12cc99df0ac1b3f5cfff4
Emir Öksüz <111661351+oksuzemir@users.noreply.github.com>
2023-05-01 12:51:29 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+window.addEventListener("load", function () {
+  const swiper = new Swiper(".swiper", {
+    slidesPerView: 2.25,
+    spaceBetween: 32,
+    slideActiveClass: "active",
+    navigation: {
+      nextEl: ".next",
+      prevEl: ".prev",
+    },
+    pagination: {
+      el: ".pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      1450: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      1200: {
+        slidesPerView: 2.25,
+        spaceBetween: 40,
+      },
+      500: {
+        slidesPerView: 1.5,
+        spaceBetween: 50,
+      },
+      200: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+    },
+  });
+});
